8ce8c0bb90e441a8d84a2c07c7113a8ef0a6f105
keskinemine <85511772+keskinemine@users.noreply.github.com>
2021-11-07 20:54:09 +0300

    Add files via upload

---
+++ systematic-grid/action_page.php
@@ -0,0 +1,15 @@
+<?php
+
+$firstname= $_POST["firstname"];
+$lastname= $_POST["lastname"];
+$subject= $_POST["subject"];
+$subjectarea= $_POST["subjectarea"];
+
+echo "Firstname: " . $firstname;
+echo "<br>";
+echo "Lastname: " . $lastname;
+echo "<br>";
+echo "Subject: " . $subject;
+echo "<br>";
+echo "Message: " . $subjectarea;
+echo "<br>";
